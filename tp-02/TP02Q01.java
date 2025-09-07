796ec77a73bd7a82e455454cfb4f6919a3ba15fa
Gabriel Nogueira <gabrielmatosnogueiracompsci@gmail.com>
2025-09-07 16:45:47 -0300

    Update TP02Q01.java

--- tp-02/TP02Q01.java
+++ tp-02/TP02Q01.java
@@ -1,129 +1,217 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class TP02Q01 
 {
+    private static boolean boolArg(String arg) 
+    {
+        boolean result = false;
+        if (arg.equals("1")) {
+            result = true;
+        }
+        return result;
+    }
+
+    private static String boolToString(boolean arg) 
+    {
+        String result = "0";
+        if (arg) {
+            result = "1";
+        }
+        return result;
+    }
+
+    private static int charToInt(char c) 
+    {
+        return c - '0';
+    }
+
+    private static boolean or(List<String> args) 
+    {
+        boolean resp = false;
+        int i = 0;
+        while (i < args.size()) 
+        {
+            String arg = args.get(i);
+            if(boolArg(arg)) 
+            {
+                resp = true;
+            }
+            i++;
+        }
+        return resp;
+    }
+
+    private static boolean and(List<String> args) 
+    {
+        boolean resp = true;
+        int i = 0;
+        while (i < args.size()) 
+        {
+            String arg = args.get(i);
+            if(!boolArg(arg)) 
+            {
+                resp = false;
+            }
+            i++;
+        }
+        return resp;
+    }
+
+    private static boolean not(boolean arg) 
+    {
+        boolean result = true;
+        if (arg) {
+            result = false;
+        }
+        return result;
+    }
+
+    private static boolean valueOfArgument(List<Integer> arguments, char letter) 
+    {
+        int idx = 0;
+        char lower = letter;
+        if (letter >= 'A' && letter <= 'Z') {
+            lower = (char)(letter + 32);
+        }
+        idx = lower - 'a';
+        boolean result = false;
+        if (arguments.get(idx) == 1) {
+            result = true;
+        }
+        return result;
+    }
+
+    private static int getClosedIndex(String cmd, int i) 
+    {
+        int open = 0;
+        int result = i;
+        while (result < cmd.length()) 
+        {
+            char c = cmd.charAt(result);
+            if(c=='(') 
+            {
+                open++;
+            } 
+            else if(c == ')') 
+            {
+                open--;
+            }
+            if(open == 0) 
+            {
+                return result;
+            }
+            result++;
+        }
+        return result;
+    }
+
+    private static int getVariables(String cmd, List<Integer> arguments) 
+    {
+        int i = 0;
+        int qtd = 0;
+        while (cmd.charAt(i) != ' ') 
+        {
+            qtd = (qtd * 10) + charToInt(cmd.charAt(i));
+            i++;
+        }
+        i++;
+        int j = 0;
+        while (j < qtd) 
+        {
+            arguments.add(charToInt(cmd.charAt(i)));
+            i += 2;
+            j++;
+        }
+        return i;
+    }
+
+    private static void doOperations(Stack<String> p) 
+    {
+        List<String> listOfArgs = new ArrayList<>();
+        while(p.peek().charAt(0) == '0' || p.peek().charAt(0) == '1') 
+        {
+            listOfArgs.add(p.pop());
+        }
+
+        String operation = p.pop();
+        boolean res = false;
+        if (operation.equals("not")) {
+            res = not(boolArg(listOfArgs.get(0)));
+        } else if (operation.equals("and")) {
+            res = and(listOfArgs);
+        } else {
+            res = or(listOfArgs);
+        }
+        p.push(boolToString(res));
+    }
+
+    private static String methods(String cmd) 
+    {
+        String result = "not";
+        if(cmd.length() == 2 && cmd.charAt(0) == 'o' && cmd.charAt(1) == 'r') 
+        {
+            result = "or";
+        } 
+        else if(cmd.length() == 2 && cmd.charAt(0) == 'n' && cmd.charAt(1) == 'd') 
+        {
+            result = "and";
+        }
+        return result;
+    }
+
+    private static void parse(String cmd, List<Integer> arguments, Stack<String> p, int i) 
+    {
+        if(i >= cmd.length()) 
+        {
+            return;
+        }
+
+        char c = cmd.charAt(i);
+        int icpy = i+1;
+        if(c == '(') 
+        {
+            int j = getClosedIndex(cmd, i);
+            String op = "";
+            int k = i-2;
+            while (k < i) {
+                op = op + cmd.charAt(k);
+                k++;
+            }
+            p.push(methods(op));
+            String sub = "";
+            int m = i+1;
+            while (m < j) {
+                sub = sub + cmd.charAt(m);
+                m++;
+            }
+            parse(sub, arguments, p, 0);
+            doOperations(p);
+            icpy = j;
+        } 
+        else if(c >= 'A' && c <= 'Z') 
+        {
+            p.push(boolToString(valueOfArgument(arguments, c)));
+        }
+
+        parse(cmd, arguments, p, icpy);
+    }
+
     public static void main(String[] args) 
     {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        
-        while(!(input.length() == 3 && input.charAt(0) == 'F' && input.charAt(1) == 'I' && input.charAt(2) == 'M'))
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        while(!text.equals("0")) 
         {
-            inputVogais(input);
-            System.out.print(" ");
-            inputConsoantes(input);
-            System.out.print(" ");
-            inputInt(input);
-            System.out.print(" ");
-            inputReal(input);
-            System.out.print(" ");
-            System.out.println();
-            input = scanner.nextLine();
+            Stack<String> p = new Stack<>();
+            List<Integer> arguments = new ArrayList<>();
+            int i = getVariables(text, arguments);
+            parse(text, arguments, p, i);
+            System.out.println(p.pop());
+            text=sc.nextLine();
         }
-
-        scanner.close();
-    }
-
-    public static void inputVogais(String str) 
-    {
-        int i=0;
-        boolean verify = true;
-        while(i!=str.length() && verify)
-        {
-            // Negacao da expressao, vogal negada é consoante
-            if(!(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u' || str.charAt(i)=='A' || str.charAt(i)=='E' || str.charAt(i)=='I' || str.charAt(i)=='O' || str.charAt(i)=='U'))
-            {
-                verify = false;
-            }
-            i=i+1;
-        }
-
-        if(verify)
-        {
-            System.out.print("SIM");
-        }
-        else
-        {
-            System.out.print("NAO");
-        }
-    }
-
-    public static void inputConsoantes(String str) 
-    {
-        int i=0;
-        boolean verify = true;
-        while(i!=str.length() && verify)
-        {
-            if((str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u' || str.charAt(i)=='A' || str.charAt(i)=='E' || str.charAt(i)=='I' || str.charAt(i)=='O' || str.charAt(i)=='U') || !(str.charAt(i)>='A' && str.charAt(i)<='Z' || str.charAt(i)>='a' && str.charAt(i)<='z'))
-            {
-                verify = false;
-            }
-            i=i+1;
-        }
-
-        if(verify)
-        {
-            System.out.print("SIM");
-        }
-        else
-        {
-            System.out.print("NAO");
-        }
-    }
-
-    public static void inputInt(String str) 
-    {
-        int i=0;
-        boolean verify = true;
-        while(i!=str.length() && verify)
-        {
-            if(!(str.charAt(i)>='0' && str.charAt(i)<='9'))
-            {
-                verify = false;
-            }
-            i=i+1;
-        }
-
-        if(verify)
-        {
-            System.out.print("SIM");
-        }
-        else
-        {
-            System.out.print("NAO");
-        }
-    }
-
-    public static void inputReal(String str)
-    {
-        int i=0;
-        int countPontoVirgula = 0;
-        boolean verify = true;
-        while(i!=str.length() && verify)
-        {
-            if(!(str.charAt(i)>='0' && str.charAt(i)<='9' || str.charAt(i)=='.' || str.charAt(i)==','))
-            {
-                verify = false;
-            }
-            if(str.charAt(i)=='.' || str.charAt(i)==',')
-            {
-                countPontoVirgula = countPontoVirgula + 1;
-            }
-            i=i+1;
-        }
-
-        if(countPontoVirgula>1)
-        {
-            verify = false;
-        }
-
-        if(verify)
-        {
-            System.out.print("SIM");
-        }
-        else
-        {
-            System.out.print("NAO");
-        }
+        sc.close();
     }
 }
