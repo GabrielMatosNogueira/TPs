f4daa59155fc7a56e97f7ed6baf779060a019631
GabrielMatosNogueira <gabrielmatosnogueiracompsci@gmail.com>
2025-10-01 12:03:09 -0300

    faltou_Criar_Array_Games_e_Set_por_Funcao

--- tp-04/Q01/Game.java
+++ tp-04/Q01/Game.java
@@ -18,21 +18,23 @@ public class Game
     private String genres;
     private String tags;
 
-    public void getId()                                             {System.out.println(this.id);}
-    public void getName()                                           {System.out.println(this.name);}
-    public void getReleaseDate()                                    {System.out.println(this.releaseDate);}
-    public void getEstimatedOwners()                                {System.out.println(this.price);}
-    public void getPrice()                                          {System.out.println(this.estimatedOwners);}
-    public void getSupportedLanguages()                             {System.out.println(this.supportedLanguages);}
-    public void getMetaCritic()                                     {System.out.println(this.metaCritic);}
-    public void getUserScore()                                      {System.out.println(this.userScore);}
-    public void getAchievements()                                   {System.out.println(this.achievements);}
-    public void getPublishers()                                     {System.out.println(this.publishers);}
-    public void getDevelopers()                                     {System.out.println(this.developers);}
-    public void getCategories()                                     {System.out.println(this.categories);}
-    public void getGenres()                                         {System.out.println(this.genres);}
-    public void getTags()                                           {System.out.println(this.tags);}
+    // Getters
+    public int getId()                                              {return (this.id);}
+    public String getName()                                         {return(this.name);}
+    public String getReleaseDate()                                  {return(this.releaseDate);}
+    public int getEstimatedOwners()                                 {return(this.estimatedOwners);}
+    public double getPrice()                                        {return(this.price);}
+    public String getSupportedLanguages()                           {return(this.supportedLanguages);}
+    public int getMetaCritic()                                      {return(this.metaCritic);}
+    public int getUserScore()                                       {return(this.userScore);}
+    public int getAchievements()                                    {return(this.achievements);}
+    public String getPublishers()                                   {return(this.publishers);}
+    public String getDevelopers()                                   {return(this.developers);}
+    public String getCategories()                                   {return(this.categories);}
+    public String getGenres()                                       {return(this.genres);}
+    public String getTags()                                         {return(this.tags);}
 
+    // Setters
     public void setId(int id)                                       {this.id = id;}
     public void setName(String name)                                {this.name = name;}
     public void setReleaseDate(String date)                         {this.releaseDate = date;}
@@ -47,4 +49,106 @@ public class Game
     public void setCategories(String categories)                    {this.categories = categories;}
     public void setGenres(String genres)                            {this.genres = genres;}
     public void setTags(String tags)                                {this.tags = tags;}
+
+    // Constructors
+    public Game(int id, String name, String releaseDate, int estimatedOwners, double price, String supportedLanguages,
+            int metaCritic, int userScore, int achievements, String publishers, String developers,
+            String categories, String genres, String tags) 
+    {
+        this.id = id;
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.estimatedOwners = estimatedOwners;
+        this.price = price;
+        this.supportedLanguages = supportedLanguages;
+        this.metaCritic = metaCritic;
+        this.userScore = userScore;
+        this.achievements = achievements;
+        this.publishers = publishers;
+        this.developers = developers;
+        this.categories = categories;
+        this.genres = genres;
+        this.tags = tags;
+    }
+
+    public Game() 
+    {
+        this(-1, null, null, -1, -1.0, null, -1, -1, -1, null, null, null, null, null);
+    }
+
+    /**
+     * @brief 
+     * @param filename
+     * @return
+     * @throws java.io.IOException
+     */
+    public Game setterInstanceValues(Game game, String)
+    {
+        int i = 0;
+        while(i < line.length())
+        {
+            // Obter o ID do jogo
+            
+
+            i=i+1;
+        }
+    }
+
+    /**
+     * @brief Programa principal
+     * @details Irá ler as linhas do arquivo csv e irá
+     *          enviar para outras funcoes para trata-
+     *          mento
+     * @param args
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws java.io.IOException
+    {
+        String filenamae = "games.csv";
+        String line = "";
+        Scanner sc = new Scanner(System.in);
+        Scanner csvScanner = new Scanner(Paths.get(filenamae));
+        boolean continueReading = true;
+        int gamesCounter = 0;
+        int i = 0;
+
+        // Pular o cabeçalho
+        if(csvScanner.hasNext())
+        {
+            csvScanner.nextLine();
+        }
+
+        // Contador numero de jogos
+        while(csvScanner.hasNext() && continueReading)
+        {
+            line = csvScanner.nextLine();
+            if(line.charAt(0) != 'F' && line.charAt(1) != 'I' && line.charAt(2) != 'M' && line.length() != 3)
+            {
+                gamesCounter++;
+            }
+            else
+            {
+                continueReading = false;
+            }
+        }
+
+        String[] games = new String[gamesCounter];
+        Game[] game = new Game[gamesCounter];
+
+        while(csvScanner.hasNext() && continueReading)
+        {
+            line = csvScanner.nextLine();
+            if(line.charAt(0) != 'F' && line.charAt(1) != 'I' && line.charAt(2) != 'M' && line.length() != 3)
+            {
+                game[i] = setterInstanceValues(game, line);
+                i = i+1;
+            }
+            else
+            {
+                continueReading = false;
+            }
+        }
+
+        csvScanner.close();
+    }
 }
